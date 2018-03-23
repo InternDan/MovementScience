ce07ad9a5d176623956ce9412e0befd9e046bf18
Dan Leib <daniel.leib@gmail.com>
2018-03-23 08:58:53 -0500

    added post process execution activity

--- app/src/main/java/com/home/buffa/movementscience/postProcessExecute.java
+++ app/src/main/java/com/home/buffa/movementscience/postProcessExecute.java
@@ -1,14 +1,26 @@
 package com.home.buffa.movementscience;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class postProcessExecute extends Activity {
 
+    Uri vid1URI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post_process_execute);
+        setContentView(R.layout.activity_poat_process_execute);
+
+        Intent intentReceive = getIntent();
+        String vidPath = intentReceive.getExtras().getString("vidPath1");
+        vid1URI = Uri.parse(vidPath);
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
     }
 }
